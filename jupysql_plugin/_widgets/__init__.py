3de3b0247f5fe92e2a7ef48da38ce729b1106fc3
yafimvo <yafimvo@gmail.com>
2023-06-05 12:00:16 +0300

    connectorwidget removed

--- jupysql_plugin/_widgets/__init__.py
+++ jupysql_plugin/_widgets/__init__.py
@@ -1,4 +1,3 @@
 from jupysql_plugin._widgets._widgets import FormWidget, TableWidget
-from jupysql_plugin._widgets.connector_widget import ConnectorWidget
 
-__all__ = ["ConnectorWidget", "FormWidget", "TableWidget"]
+__all__ = ["FormWidget", "TableWidget"]
