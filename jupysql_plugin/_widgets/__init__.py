51f1b9e6067d83faa1e2c476a6c67c05dda7bb9b
yafimvo <yafimvo@gmail.com>
2023-06-05 10:35:36 +0300

    lint

--- jupysql_plugin/_widgets/__init__.py
+++ jupysql_plugin/_widgets/__init__.py
@@ -1,4 +1,5 @@
 from jupysql_plugin._widgets._widgets import FormWidget, TableWidget
+
 # from jupysql_plugin._widgets.connector_widget import ConnectorWidget
 
 __all__ = ["FormWidget", "TableWidget"]
